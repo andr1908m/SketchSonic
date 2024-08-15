af87475cb31f87d21fb7a10749e237ab9cc7914d
Andres Rodriguez <contact@andresr.rocks>
2024-08-14 22:33:20 -0400

    deps must be installed first before composer

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY . .
 
 RUN chmod 777 /var/www/html/install.sh
 RUN /var/www/html/install.sh
-RUN apt install -y php-xml php-xdebug
 RUN composer analyse
 RUN composer coverage
 
--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 apt update
 apt install -y software-properties-common
 add-apt-repository ppa:ondrej/php
+apt install -y php-xml php-xdebug
 apt install -y composer
 composer install
